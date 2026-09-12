99012c6565658d3df804d947a5bc0d3cd05f5fe9
Aravindhan <104446384+Aravindh-dev12@users.noreply.github.com>
2026-09-12 14:50:09 +0530

    db: export institutional continuity primitives

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -15,4 +15,5 @@ if (process.env.NODE_ENV !== 'production') {
 export * from './events.js';
 export * from './host-receipts.js';
 export * from './continuity.js';
+export * from './institutional.js';
 export * from '@prisma/client';
